58e3dbbcfa6a6f43142f6258639ca84eb37bd52a
Sneyder Ospina <sneydero@hotmail.com>
2024-10-07 20:06:29 -0500

    Inputs variables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,7 @@ import { UserComponent } from "./user/user.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'first-angular-app';
+  users = DUMMY_USERS;
 }
